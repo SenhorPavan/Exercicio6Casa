7821647a16c4c2eff23400b58b14cc35f9838434
SenhorPavan <gabriel_santos@estudante.sc.snaia.br>
2021-04-16 12:50:32 -0300

    tarefa de casa

---
+++ src/maior/numero/Exercicio6.java
@@ -0,0 +1,16 @@
+
+package maior.numero;
+
+
+public class Exercicio6 {
+
+
+    public static void main(String[] args) {
+        
+       MaiorNumero n1 = new MaiorNumero(54, 53);
+       n1.ImprimaMaior();
+        
+        
+    }
+    
+}
---
+++ src/maior/numero/MaiorNumero.java
@@ -0,0 +1,32 @@
+
+package maior.numero;
+
+
+
+public class MaiorNumero {
+    
+    public MaiorNumero(int num1, int num2){
+        
+        this.num1 = num1;
+        this.num2 = num2;
+
+    }
+   
+    int num1;
+    int num2;
+
+    public void ImprimaMaior(){
+        
+        if(num1>num2){
+            System.out.println("O numero maior é " + num1);
+        }else{
+            System.out.println("O nuemro maior é " + num2);
+        }
+        
+    }
+        
+
+
+
+    
+}
